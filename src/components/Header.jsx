3a09550337974090b1630b93377f9e4d162e9190
KKumarCoder <krishnakumar.snm004@gmail.com>
2026-08-26 21:57:19 +0530

    Update student login app link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -974,7 +974,7 @@ const Header = () => {
               </div>
 
               <a
-                href={ERP_LOGIN_URL}
+                href="https://play.google.com/store/apps/details?id=com.oneschool.erp"
                 className="srps-erp-pill"
                 aria-label="Student Login"
               >
